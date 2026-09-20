85b3b2a80f65e0b9fda7372610a000847bd37538
Snehalatha Reddy <127225408+snehachan169@users.noreply.github.com>
2026-09-19 20:51:52 -0500

    Fix supabaseUrl in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PAWBUDGET_CONFIG = {
-  supabaseUrl: "https://ginyfnkapjdmliwoyjsa.supabase.co",
+  supabaseUrl: "https://ginyfnkapjdmliwojysa.supabase.co",
   supabaseKey: "sb_publishable_TWalQx_taGiWXzefsYiPag_ww1yH_26",
   siteUrl: "https://snehachan169.github.io/PawBudget/"
 };
