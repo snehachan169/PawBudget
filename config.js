239e599b154da2a4e9e4d3ccdb6ae79549620c94
Snehalatha Reddy <127225408+snehachan169@users.noreply.github.com>
2026-09-21 13:24:28 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.PAWBUDGET_CONFIG = {
+  supabaseUrl: "https://ginyfnkapjdmliwojysa.supabase.co",
+  supabaseKey: "sb_publishable_TWalQx_taGiWXzefsYiPag_ww1yH_26",
+  siteUrl: "https://snehachan169.github.io/PawBudget/",
+  ownerEmail: "asnehachan169@gmail.com"
+};
